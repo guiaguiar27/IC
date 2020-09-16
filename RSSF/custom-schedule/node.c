6723826e5f251b6f2fd516e878a23e53c6b56dbb
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2020-09-16 20:53:54 -0300

    Update node.c

--- RSSF/custom-schedule/node.c
+++ RSSF/custom-schedule/node.c
@@ -142,7 +142,7 @@ PROCESS_THREAD(node_process, ev, data)
 
   if(node_id == 1) {  /* Running on the root? */
     NETSTACK_ROUTING.root_start(); 
-    init_MADJ(Matriz); 
+    tsch_neighbour_maping(); 
   }
 
   /* Main loop */
