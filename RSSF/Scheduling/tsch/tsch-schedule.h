d363dbdef3e3760bed50fa4d407a5c2297a8ad37
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2020-10-07 20:56:08 -0300

    Update tsch-schedule.h

--- RSSF/Scheduling/tsch/tsch-schedule.h
+++ RSSF/Scheduling/tsch/tsch-schedule.h
@@ -158,5 +158,9 @@ struct tsch_slotframe *tsch_schedule_slotframe_head(void);
  */
 struct tsch_slotframe *tsch_schedule_slotframe_next(struct tsch_slotframe *sf);
 
+void  sort_links(int **coordenadas); 
+int SCHEDULE(int **adj);  
+int *alocaPacotes(int num_no, int **adj);  
+void executa(int **aloca_canal, int tempo, int **mapa_graf_conf, int *pacote_entregue, int raiz, int *pacotes); 
 #endif /* __TSCH_SCHEDULE_H__ */
 /** @} */
