77b6cccfbeb2bb71e6b6987228587c7b3d729dfb
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2020-11-20 12:47:59 -0300

    .

--- RSSF/Scheduling/tsch/conf.h
+++ RSSF/Scheduling/tsch/conf.h
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 
 
-
-
 int **geraMaching(int *pacotes, int **mat_adj, int **graf_conf, int **mapa_graf_conf, int tam, int tam_rede, int node);
 void selecao(int **conf, int pai, int tam);
 int **mapGraphConf(int **mat, int tam_no, int tam_aresta);
