217f62731d5251b5402d7a9c391deccce994fa64
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2020-12-29 15:23:26 -0300

    .

--- RSSF/tsch/conf.c
+++ RSSF/tsch/conf.c
@@ -58,12 +58,11 @@ int **geraMaching(int *pacotes, int **mat_adj, int **graf_conf, int **mapa_graf_
     * resultado: matriz de adjacência do matching
     */
      int x, y ; //, vetor[tam][2] , maior_peso, cont = 1, flg = 1;
-    int resultado[tam_rede][tam_rede];
     
-    // int **resultado = (int**)malloc(tam_rede * sizeof(int*));
-    // for(x = 0; x < tam_rede; x++){
-    //     resultado[x] = (int*)malloc(tam_rede * sizeof(int));
-    // } 
+     int **resultado = (int**)malloc(tam_rede * sizeof(int*));
+     for(x = 0; x < tam_rede; x++){
+         resultado[x] = (int*)malloc(tam_rede * sizeof(int));
+     } 
     for(x = 0; x < tam_rede; x++){ 
         for(y = 0; y < tam_rede; y++)
             resultado[x][y] = 0;
@@ -120,7 +119,7 @@ int **geraMaching(int *pacotes, int **mat_adj, int **graf_conf, int **mapa_graf_
     // }
 
     //printf("\nPassei por aqui\n");
-    return (&resultado);
+    return resultado;
 }
 
 //Armazena as arestas de um grafo e os respectivos nós q o compõem
