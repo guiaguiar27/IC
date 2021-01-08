46f9cde46ed25b647f482096be1eddeb01401976
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-01-07 21:39:49 -0300

    .

--- RSSF/tsch/conf.c
+++ RSSF/tsch/conf.c
@@ -122,11 +122,11 @@ int **geraMaching(int *pacotes, int **mat_adj, int **graf_conf, int **mapa_graf_
     
    // Preenche a matriz de adjacência com as arestas que podem transmitir ao mesmo tempo
     printf("mapa_graf_conf[x][0] = %d\n",mapa_graf_conf[x][0]);
-    for(x = 0; x < num_arestas; x++){
-        if(vetor[x][0] == 0 && pacotes[mapa_graf_conf[x][0]] > 0){
-            resultado[mapa_graf_conf[x][0]][mapa_graf_conf[x][1]] = 1;
-        }
-    }
+    // for(x = 0; x < num_arestas; x++){
+    //     if(vetor[x][0] == 0 && pacotes[mapa_graf_conf[x][0]] > 0){
+    //         resultado[mapa_graf_conf[x][0]][mapa_graf_conf[x][1]] = 1;
+    //     }
+    // }
 
     // //printf("\nPassei por aqui\n");
     return resultado;
