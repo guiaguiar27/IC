fc85f625fb4addabfb4afdf36f7a70e240e7bf2a
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-02-02 13:59:07 -0300

    .

--- RSSF/Tree_topology/project-conf.h
+++ RSSF/Tree_topology/project-conf.h
@@ -56,7 +56,11 @@
 //#define TSCH_CALLBACK_PACKET_READY my_callback_packet_ready
 
  // conf with ipv6 
-// #define NETSTACK_CONF_WITH_IPV6  1
+#define NETSTACK_CONF_WITH_IPV6  1  
+// messages to enable ND 
+#define UIP_CONF_ND6_SEND_NS 1  
+#define UIP_CONF_ND6_SEND_NA 1  
+#define UIP_CONF_ND6_SEND_RA 1 
 
 /* Size of the application-specific schedule; a number relatively prime to the hopseq length */
 #define APP_SLOTFRAME_SIZE 17
