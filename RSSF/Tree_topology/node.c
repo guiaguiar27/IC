bc6e57b9e7d4079ed57080965a6755e3ce45b3cb
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-02-02 22:11:01 -0300

    .

--- RSSF/Tree_topology/node.c
+++ RSSF/Tree_topology/node.c
@@ -164,30 +164,26 @@ rx_packet(struct simple_udp_connection *c,
 
 PROCESS_THREAD(node_process, ev, data)
 {
- static struct simple_udp_connection udp_conn;
+  static struct simple_udp_connection udp_conn;
   static struct etimer periodic_timer;
   static uint32_t seqnum;
- uip_ipaddr_t dst;   
-  //uip_ds6_nbr_t *nbr = NULL ;  
+  uip_ipaddr_t dst;   
   PROCESS_BEGIN();
   initialize_tsch_schedule(); 
-  look_above_table();
   /* Initialization; `rx_packet` is the function for packet reception */
   simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, rx_packet);
   etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);
   
    if(node_id == 1) {  /* Running on the root? */
      NETSTACK_ROUTING.root_start(); 
-   }   
-  //NETSTACK_ROUTING.init(); 
+   }    
  // NETSTACK_MAC.init();
  // NETSTACK_MAC.on();
 
   /* Main loop */ 
   while(1) { 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer)); 
-    //SCHEDULE_static();  
-    //nbr = uip_ds6_nbr_lookup(const uip_ipaddr_t *ipaddr);  
+    //SCHEDULE_static();    
     
     if(NETSTACK_ROUTING.node_is_reachable()
        && NETSTACK_ROUTING.get_root_ipaddr(&dst)){
