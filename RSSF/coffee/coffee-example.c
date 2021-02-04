c932555cf0e49551f763588c623b6d63fcf0e1e8
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-02-04 13:54:14 -0300

    t

--- RSSF/coffee/coffee-example.c
+++ RSSF/coffee/coffee-example.c
@@ -2,7 +2,8 @@
 #include "contiki.h"  
 #include <stdio.h>  
 #include "sys/log.h"   
-#include "os/storage/cfs/cfs.h"  
+#include "os/storage/cfs/cfs.h"   
+#include "sys/node-id.h"  
 //#define arquivo "teste.txt"
 PROCESS(coffee_test_process, "Coffee test process");
  AUTOSTART_PROCESSES(&coffee_test_process);
@@ -11,27 +12,39 @@ PROCESS(coffee_test_process, "Coffee test process");
  {
    PROCESS_BEGIN();
      
-   /* step 1 */
-    char message[32];
+   /* step 1 */   
+   char message[3];
+   if(node_id == 1 ){ 
+     strcpy(message,"1\n");
+   } 
+   else if(node_id == 2 ){ 
+     strcpy(message,"2\n");
+   } 
+  else if(node_id == 3 ){ 
+     strcpy(message,"3\n");
+   }   
+    
     char buf[100];    
-    char *arquivo = " msg file"; 
-    // struct cfs_dir *dirp = NULL ;  
-    // strcpy(dirp->state,endereco_teste); 
+    char *arquivo = " "; 
         
-    strcpy(message,"#1.hello world.");
     strcpy(buf,message); 
 
     printf("step 1: %s\n", buf );  
-   // int dir_write = cfs_opendir(dirp, arquivo); 
-   // if(dir_write != -1 ) printf("Open dir\n");
     int fd_write = cfs_open(arquivo, CFS_WRITE | CFS_APPEND);
     if(fd_write != -1) {
         int n = cfs_write(fd_write, message, sizeof(message));
         cfs_close(fd_write); 
         if(n)   
             printf("step 2: successfully written to cfs. wrote %i bytes\n", n);
-    }   
+        }   
 
+    else {
+        printf("ERROR: could not write to memory in step 2.\n");
+    }
+    int fd_read = cfs_open(arquivo, CFS_READ); 
+    if(fd_read != -1) {
+        cfs_read(fd_read, buf, sizeof(message));
+        printf("%s\n", buf); 
     else {
         printf("ERROR: could not write to memory in step 2.\n");
     }
