087123c45cb2d66ebdf60174f377aced74d5aaed
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-02-23 20:32:01 -0300

    .

--- RSSF/Cooja_mote/custom-schedule/node.c
+++ RSSF/Cooja_mote/custom-schedule/node.c
@@ -73,11 +73,11 @@ static linkaddr_t coordinator_addr =  {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0
 int  verify = 0, aux_id  ;  
 
 
-static unsigned long
-to_seconds(uint64_t time)
-{
-  return (unsigned long)(time / ENERGEST_SECOND);
-}
+// static unsigned long
+// to_seconds(uint64_t time)
+// {
+//   return (unsigned long)(time / ENERGEST_SECOND);
+// }
 
 #if NBR_TSCH 
 static void init_broad(void){  
@@ -239,20 +239,20 @@ PROCESS_THREAD(node_process, ev, data)
       // mudanca  
     #endif 
 
-    energest_flush();
+    // energest_flush();
 
-    printf("\nEnergest:\n");
-    printf(" CPU          %4lus LPM      %4lus DEEP LPM %4lus  Total time %lus\n",
-           to_seconds(energest_type_time(ENERGEST_TYPE_CPU)),
-           to_seconds(energest_type_time(ENERGEST_TYPE_LPM)),
-           to_seconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)),
-           to_seconds(ENERGEST_GET_TOTAL_TIME()));
-    printf(" Radio LISTEN %4lus TRANSMIT %4lus OFF      %4lus\n",
-           to_seconds(energest_type_time(ENERGEST_TYPE_LISTEN)),
-           to_seconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
-           to_seconds(ENERGEST_GET_TOTAL_TIME()
-                      - energest_type_time(ENERGEST_TYPE_TRANSMIT)
-                      - energest_type_time(ENERGEST_TYPE_LISTEN)));
+    // printf("\nEnergest:\n");
+    // printf(" CPU          %4lus LPM      %4lus DEEP LPM %4lus  Total time %lus\n",
+    //        to_seconds(energest_type_time(ENERGEST_TYPE_CPU)),
+    //        to_seconds(energest_type_time(ENERGEST_TYPE_LPM)),
+    //        to_seconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)),
+    //        to_seconds(ENERGEST_GET_TOTAL_TIME()));
+    // printf(" Radio LISTEN %4lus TRANSMIT %4lus OFF      %4lus\n",
+    //        to_seconds(energest_type_time(ENERGEST_TYPE_LISTEN)),
+    //        to_seconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
+    //        to_seconds(ENERGEST_GET_TOTAL_TIME()
+    //                   - energest_type_time(ENERGEST_TYPE_TRANSMIT)
+    //                   - energest_type_time(ENERGEST_TYPE_LISTEN)));
     
      
     //verify_packs();
