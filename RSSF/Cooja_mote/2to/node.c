2fae3c0c761717c2908ffd80889483b10ae18098
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-03-29 16:45:55 -0300

    node.c 2to (only 3 nodes)

--- RSSF/Cooja_mote/2to/node.c
+++ RSSF/Cooja_mote/2to/node.c
@@ -55,186 +55,220 @@ PROCESS(node_process, "TSCH Schedule Node");
 AUTOSTART_PROCESSES(&node_process); 
 static linkaddr_t coordinator_addr =  {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
 
-/*
- * Note! This is not an example how to design a *good* schedule for TSCH,
- * nor this is the right place for complete beginners in TSCH.
- * We recommend using the default Orchestra schedule for a start.
- *
- * The intention of this file is instead to serve a starting point for those interested in building
- * their own schedules for TSCH that are different from Orchestra and 6TiSCH minimal.
- */
 
 /* Put all cells on the same slotframe */
 #define APP_SLOTFRAME_HANDLE 1
-#define APP_UNICAST_TIMESLOT 16 
-#define APP_CHANNEL_OFSETT 16   
+#define APP_UNICAST_TIMESLOT 4
+#define APP_CHANNEL_OFSETT 8   
 
 
-int  aux_id  ;  
+int  verify = 0, aux_id  ;  
 
 
-// static unsigned long
-// to_seconds(uint64_t time)
-// {
-//   return (unsigned long)(time / ENERGEST_SECOND);
-// }
+static unsigned long
+to_seconds(uint64_t time)
+{
+  return (unsigned long)(time / ENERGEST_SECOND);
+}
 
- 
-struct latency_structure{ // Structure to save the time when the message was send
+#if NBR_TSCH 
+static void init_broad(void){  
 
-   rtimer_clock_t timestamp;// Time when the message was send
-
-}; 
+  struct tsch_slotframe *sf_common = tsch_schedule_add_slotframe(APP_SLOTFRAME_HANDLE, APP_SLOTFRAME_SIZE);
+  uint16_t slot_offset = 0;
+  uint16_t channel_offset = 0; 
+  
+  tsch_schedule_add_link(sf_common,
+      LINK_OPTION_RX | LINK_OPTION_TX | LINK_OPTION_SHARED,
+      LINK_TYPE_ADVERTISING, &tsch_broadcast_address,
+      slot_offset, channel_offset,0);
+    
+} 
+#endif  
 
 int initialize_tsch_schedule(void){
-
- 
- 
-    LOG_PRINT("Initialize tsch schedule\nRemoving all old slotframes\n");
- 
     
-    int i, j; 
-    // APP_SLOTFRAME_SIZE
-    struct tsch_slotframe *sf_common = tsch_schedule_add_slotframe(APP_SLOTFRAME_HANDLE, APP_SLOTFRAME_SIZE);
-    uint16_t slot_offset;
-    uint16_t channel_offset;  
-    uint8_t link_options;
-    
-    slot_offset = 0;
-    channel_offset = 0;
-    int num_links = 1 ;    
     uint16_t remote_id = 0; 
-    linkaddr_t addr;  
 
-      if (node_id != 1) {
-        
-        for (i = 0 ; i <  num_links ; ++i) { 
-        
-          remote_id = 1 ; 
-         
-        
-        for(j = 0; j < sizeof(addr); j += 2) {
-            addr.u8[j + 1] = remote_id & 0xff;
-            addr.u8[j + 0] = remote_id >> 8;
-          } 
-        }  
-          slot_offset = 1 ; //random_rand() % APP_UNICAST_TIMESLOT;
-          channel_offset = 2; //random_rand() % APP_CHANNEL_OFSETT ;
-          /* Warning: LINK_OPTION_SHARED cannot be configured, as with this schedule
-          * backoff windows will not be reset correctly! */
-          link_options = LINK_OPTION_TX;
-
-          tsch_schedule_add_link(sf_common,
-              link_options,
-              LINK_TYPE_NORMAL, &addr,
-              slot_offset, channel_offset,0);
-          } 
-        else { 
-          remote_id = 2 ; 
-         
-        
-        for(j = 0; j < sizeof(addr); j += 2) {
-            addr.u8[j + 1] = remote_id & 0xff;
-            addr.u8[j + 0] = remote_id >> 8;
-          } 
-          
-          slot_offset = 1 ; //random_rand() % APP_UNICAST_TIMESLOT;
-          channel_offset = 2; //random_rand() % APP_CHANNEL_OFSETT ;
-          /* Warning: LINK_OPTION_SHARED cannot be configured, as with this schedule
-          * backoff windows will not be reset correctly! */
-          link_options = LINK_OPTION_RX;
-
-          tsch_schedule_add_link(sf_common,
-              link_options,
-              LINK_TYPE_NORMAL, &addr,
-              slot_offset, channel_offset,0);
-        }  
-          return remote_id;
-      
-    }
+    if(node_id == 1 ) { 
+      LOG_PRINT("Initialize tsch schedule\n"); 
+      tsch_schedule_remove_all_slotframes(); 
+      LOG_PRINT("Remove all slotframes");
+      struct tsch_slotframe *sf_common = tsch_schedule_add_slotframe(APP_SLOTFRAME_HANDLE, APP_SLOTFRAME_SIZE);  
+      int  j; 
+      // APP_SLOTFRAME_SIZE
+      uint16_t slot_offset = 1 ;
+      uint16_t channel_offset = 1 ;  
      
-      
-  
+      linkaddr_t addr;   
+      remote_id = 2 ;  // recebe do dois  
+      for(j = 0; j < sizeof(addr); j += 2) {
+            addr.u8[j + 1] = remote_id & 0xff;
+            addr.u8[j + 0] = remote_id >> 8;
+          } 
+      tsch_schedule_add_link(sf_common,
+              LINK_OPTION_RX,
+              LINK_TYPE_NORMAL, &addr,
+              slot_offset, channel_offset,0);
+      return remote_id;  
+        
+    } 
+    else if(node_id == 2){  
+      LOG_PRINT("Initialize tsch schedule\n"); 
+      tsch_schedule_remove_all_slotframes(); 
+      LOG_PRINT("Remove all slotframes");
+      struct tsch_slotframe *sf_common = tsch_schedule_add_slotframe(APP_SLOTFRAME_HANDLE, APP_SLOTFRAME_SIZE);  
+      int  j; 
+      // APP_SLOTFRAME_SIZE
+      uint16_t slot_offset = 1 ;
+      uint16_t channel_offset = 1 ;  
+     
+      linkaddr_t addr;   
+      remote_id = 1 ;  // recebe do dois  
+      for(j = 0; j < sizeof(addr); j += 2) {
+            addr.u8[j + 1] = remote_id & 0xff;
+            addr.u8[j + 0] = remote_id >> 8;
+          } 
+      tsch_schedule_add_link(sf_common,
+              LINK_OPTION_TX,
+              LINK_TYPE_NORMAL, &addr,
+              slot_offset, channel_offset,0);  
+      // receive from node 3 
+      slot_offset = 2 ;
+      channel_offset = 2 ;  
+      remote_id = 3 ;  // recebe do dois  
+      for(j = 0; j < sizeof(addr); j += 2) {
+            addr.u8[j + 1] = remote_id & 0xff;
+            addr.u8[j + 0] = remote_id >> 8;
+          } 
+      tsch_schedule_add_link(sf_common,
+              LINK_OPTION_RX,
+              LINK_TYPE_NORMAL, &addr,
+              slot_offset, channel_offset,0);
+      return remote_id;  
+        
 
-static void 
-broadcast_recv(struct broadcast_conn *c, const linkaddr_t *from)
-{
-  struct latency_structure msg;
-  rtimer_clock_t latency;
-
-  packetbuf_copyto( &msg ); // Copy the message from the packet buffer to the structure called msg
-
-#if TIMESYNCH_CONF_ENABLED
-  latency = timesynch_time() - msg.timestamp;
-#else
-  latency = 0;
-#endif
-
-  printf("broadcast message received from %d.%d with latency %lu ms\n",
-         from->u8[0], from->u8[1], (1000L * latency) / RTIMER_ARCH_SECOND );
+    }
+    else if(node_id == 3){ 
+      LOG_PRINT("Initialize tsch schedule\n"); 
+      tsch_schedule_remove_all_slotframes(); 
+      LOG_PRINT("Remove all slotframes");
+      struct tsch_slotframe *sf_common = tsch_schedule_add_slotframe(APP_SLOTFRAME_HANDLE, APP_SLOTFRAME_SIZE);  
+      int  j; 
+      // APP_SLOTFRAME_SIZE
+      uint16_t slot_offset = 2 ;
+      uint16_t channel_offset = 2 ;  
+     
+      linkaddr_t addr;   
+      remote_id = 2;  // recebe do dois  
+      for(j = 0; j < sizeof(addr); j += 2) {
+            addr.u8[j + 1] = remote_id & 0xff;
+            addr.u8[j + 0] = remote_id >> 8;
+          } 
+      tsch_schedule_add_link(sf_common,
+              LINK_OPTION_TX,
+              LINK_TYPE_NORMAL, &addr,
+              slot_offset, channel_offset,0); 
+    }
 } 
+     
+    
+static void
+rx_packet(struct simple_udp_connection *c,
+          const uip_ipaddr_t *sender_addr,
+          uint16_t sender_port,
+          const uip_ipaddr_t *receiver_addr,
+          uint16_t receiver_port,
+          const uint8_t *data,
+          uint16_t datalen)
+{
+  uint32_t seqnum;
 
-static const struct broadcast_callbacks broadcast_call = {broadcast_recv};
-static struct broadcast_conn broadcast; 
+  if(datalen >= sizeof(seqnum)) {
+    memcpy(&seqnum, data, sizeof(seqnum));
+
+    LOG_INFO("Received from ");
+    LOG_INFO_6ADDR(sender_addr);
+    LOG_INFO_(", seqnum %" PRIu32 "\n", seqnum);
+  }
+}
 
 PROCESS_THREAD(node_process, ev, data)
-{ 
-  struct latency_struct *msg ;  
+{
+  static struct simple_udp_connection udp_conn;
   static struct etimer periodic_timer; 
+  static uint32_t seqnum;
+  uip_ipaddr_t dst;   
   
-  PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
-
-  PROCESS_BEGIN();   
-
-  broadcast_open(&broadcast, 129, &broadcast_call);
-  tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
-  /* Initialization; `rx_packet` is the function for packet reception */
-  simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, rx_packet);
-  etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);  
-  // para mudar o slotframe;  
-   if(node_id == 1) {  /* Running on the root? */
-     NETSTACK_ROUTING.root_start();
-   } 
-  // ativa os protocolos da camda de rede 
-  NETSTACK_MAC.on(); 
-
-
-  /* Main loop */
-  while(1) { 
-    aux_id = initialize_tsch_schedule();  
-    LOG_INFO("%d \n",aux_id);  
-    #if TIMESYNCH_CONF_ENABLED
-      msg->timestamp = timesynch_time();  
-    #else
-      msg->timestamp = 0;
-    #endif
-   
-    // energest_flush();
-
-    // printf("\nEnergest:\n");
-    // printf(" CPU          %4lus LPM      %4lus DEEP LPM %4lus  Total time %lus\n",
-    //        to_seconds(energest_type_time(ENERGEST_TYPE_CPU)),
-    //        to_seconds(energest_type_time(ENERGEST_TYPE_LPM)),
-    //        to_seconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)),
-    //        to_seconds(ENERGEST_GET_TOTAL_TIME()));
-    // printf(" Radio LISTEN %4lus TRANSMIT %4lus OFF      %4lus\n",
-    //        to_seconds(energest_type_time(ENERGEST_TYPE_LISTEN)),
-    //        to_seconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
-    //        to_seconds(ENERGEST_GET_TOTAL_TIME()
-    //                   - energest_type_time(ENERGEST_TYPE_TRANSMIT)
-    //                   - energest_type_time(ENERGEST_TYPE_LISTEN)));
+  PROCESS_BEGIN();  
     
-     
-    //verify_packs();
+    #if NBR_TSCH 
+      init_broad(); 
+    #endif  
 
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-     // Do not send the normal/original 'hello' message. Instead, send a message with the timestamp.
-    packetbuf_copyfrom(msg,  sizeof(*msg) ); // This message msg includes the timestamp
-    //packetbuf_copyfrom("Hello", 6);
-    broadcast_send(&broadcast);
-    printf("broadcast message sent\n");
-    etimer_set(&periodic_timer, SEND_INTERVAL);
-  }
+    tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
+    /* Initialization; `rx_packet` is the function for packet reception */
+    simple_udp_register(&udp_conn, UDP_PORT, NULL, UDP_PORT, rx_packet);
+    etimer_set(&periodic_timer, random_rand() % SEND_INTERVAL);  
+    // para mudar o slotframe;  
+    if(node_id == 1) {  /* Running on the root? */
+      NETSTACK_ROUTING.root_start();
+    } 
+    // ativa os protocolos da camda de rede 
+    NETSTACK_MAC.on(); 
 
-  PROCESS_END(); 
+
+    /* Main loop */
+    while(1) {
+      
+      #if NBR_TSCH  
+        show_nbr();   
+        // adapatação sem contar com o tempo 
+        LOG_INFO("Verify: %d \n", verify);    
+        if(verify == 0){ 
+          verify = change_slotframe(); 
+          if(verify == 1){   
+            aux_id = initialize_tsch_schedule(); 
+            if(node_id == 1) aux_id = 1 ; 
+            
+        
+        }
+        } 
+
+      find_neighbor_to_Rx(node_id,1); 
+      if(aux_id >= 1 ){ 
+        LOG_PRINT("aux_id test: %d\n", aux_id);
+        //SCHEDULE_static(); 
+      }  
+      #endif 
+
+       energest_flush();
+
+       printf("\nEnergest:\n");
+       printf(" CPU          %4lus LPM      %4lus DEEP LPM %4lus  Total time %lus\n",
+              to_seconds(energest_type_time(ENERGEST_TYPE_CPU)),
+              to_seconds(energest_type_time(ENERGEST_TYPE_LPM)),
+              to_seconds(energest_type_time(ENERGEST_TYPE_DEEP_LPM)),
+              to_seconds(ENERGEST_GET_TOTAL_TIME()));
+       printf(" Radio LISTEN %4lus TRANSMIT %4lus OFF      %4lus\n",
+              to_seconds(energest_type_time(ENERGEST_TYPE_LISTEN)),
+              to_seconds(energest_type_time(ENERGEST_TYPE_TRANSMIT)),
+              to_seconds(ENERGEST_GET_TOTAL_TIME()
+                         - energest_type_time(ENERGEST_TYPE_TRANSMIT)
+                         - energest_type_time(ENERGEST_TYPE_LISTEN)));
+
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+        if(tsch_is_associated) { 
+
+          /* Send network uptime timestamp to the network root node */
+          seqnum++;
+          LOG_INFO("Send to ");
+          LOG_INFO_6ADDR(&dst);
+          LOG_INFO_(", seqnum %" PRIu32 "\n", seqnum);
+          simple_udp_sendto(&udp_conn, &seqnum, sizeof(seqnum), &dst);
+        }
+        etimer_set(&periodic_timer, SEND_INTERVAL);
+    }
+
+  PROCESS_END();
 }
