316b390b4be44ff6a8ef7ae4ad8d73fac35dc1c9
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-04-02 12:45:24 -0300

    .

--- Dados/Testlogs/throughput.py
+++ Dados/Testlogs/throughput.py
@@ -11,7 +11,7 @@ def extract_node(source):
     for i,w in enumerate(words):
             if w == "Pckt": 
                 return words[i+1]         
-def exctract_tx_success(source):   
+def extract_tx_success(source):   
     for i,w in enumerate(words):
             if w == "Pckt": 
                 return words[i+2]
@@ -19,25 +19,39 @@ def extract_rx(source):
     for i,w in enumerate(words):
             if w == "Pckt": 
                 return words[i+3]
-def extract_tx_try(source): 
-    pass  
-nodes = [] 
-Tx_success = [] 
-Rx_success = []
+def extract_tx_try(source):  
+    for i,w in enumerate(words):
+            if w == "Tx_try": 
+                return words[i+2]
+    
+nodes = []
+Tx_total = 0
+Tx_try_sent = 0 
+Rx_total = 0  
+slotframe_size = 8
 with open('teste.txt','r') as f:
     for line in f:  
         words = line.split() 
+        if "Tx_try" in words: 
+            aux_tx_try = int(extract_tx_try(words)) 
+            if Tx_try_sent < aux_tx_try: 
+                Tx_try_sent = aux_tx_try 
         if "Pckt" in words:
             nodes.append(extract_node(words)) 
-            Tx_success.append(exctract_tx_success(words)) 
-            Rx_success.append(extract_rx(words)) 
-        
-numNodes = len(nodes)        
+            aux_tx = int(extract_tx_success(words)) 
+            if Tx_total < aux_tx: 
+                Tx_total = aux_tx 
+            aux_rx = int(extract_rx(words)) 
+            if Rx_total < aux_rx: 
+                Rx_total = aux_rx 
+                
+numNodes = len(nodes)      
+
 print("metrics simulation {} nodes".format(numNodes))
-print("nodes:", nodes[:]) 
-Tx_final = list(map(int, Tx_success)) 
-Rx_final = list(map(int, Rx_success))
-Tx_total = reduce(add, Tx_final)
-print("tx_success:", Tx_final[:]) 
-print("Total Tx:",Tx_total) 
-print("rx_success:",Rx_final[:])
+#print("nodes:", nodes[:]) 
+print("Total Tx:",Tx_total)  
+print("Total Tx try:", Tx_try_sent) 
+throughput = Tx_total/Tx_try_sent 
+print("Throughput:",throughput) 
+print("Throughput (packet_receive/numSlots):", Tx_total/slotframe_size)
+print("Total Rx:",Rx_total) 
