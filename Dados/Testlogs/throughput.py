3edb29a30ea3ac4172124ce6b7ac5180b8b73e48
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-04-06 11:30:44 -0300

    some collect

--- Dados/Testlogs/throughput.py
+++ Dados/Testlogs/throughput.py
@@ -28,16 +28,16 @@ def extract_tx_try(source):
                 return words[i+2]
     
 #network parameters  
-numNodes = 19
-slotframe_size = 17 
-
+numNodes = 7
+slotframe_size =  9
+aux_numNode = numNodes + 1
 
 nodes = [0 for i in range(numNodes)]
 # metrics per node  
-Tx_per_node = [0 for i in range(numNodes)] 
-Rx_per_node = [0 for i in range(numNodes)] 
-Tx_try_per_node = [0 for i in range(numNodes)] 
-throughput_per_node = [0 for i in range(numNodes)] 
+Tx_per_node = [0 for i in range(0,aux_numNode)] 
+Rx_per_node = [0 for i in range(0,aux_numNode)] 
+Tx_try_per_node = [0 for i in range(0,aux_numNode)] 
+throughput_per_node = [0 for i in range(0,aux_numNode)] 
 #fill nodes  
 for i in range(0,numNodes): 
     nodes.append(i) 
@@ -54,32 +54,25 @@ with open('teste.txt','r') as f:
         if "Tx_try" in words:  
             node = int(extract_node(words))
             aux_tx_try = int(extract_tx_try(words))  
-            for i in range(len(Tx_try_per_node)): 
-                if i == nodes:  
-                    if Tx_try_per_node[i] < aux_tx_try:
-                        Tx_try_per_node[i] = aux_tx_try
+            for i in range(1,6): 
+                if i == node:
+                    Tx_try_per_node[i] = aux_tx_try
             
-            if Tx_try_sent < aux_tx_try: 
-                Tx_try_sent = aux_tx_try  
-
+            
         if "Pckt" in words:
             node = int(extract_node(words))
         
             aux_tx = int(extract_tx_success(words)) 
-            if Tx_total < aux_tx: 
-                Tx_total = aux_tx 
             aux_rx = int(extract_rx(words)) 
-            if Rx_total < aux_rx: 
-                Rx_total = aux_rx  
-
+           
             Lrx = int(extract_rx(words))  
             for i in range(len(Rx_per_node)): 
-                if i == nodes:  
+                if i == node:   
                     if Rx_per_node[i] < Lrx:
                         Rx_per_node[i] = Lrx
             Ltx = int(extract_tx_success(words))  
             for i in range(len(Tx_per_node)): 
-                if i == nodes:  
+                if i == node:  
                     if Tx_per_node[i] < Ltx:
                         Tx_per_node[i] = Ltx
             
@@ -87,12 +80,30 @@ with open('teste.txt','r') as f:
 
 print("metrics simulation {} nodes".format(numNodes))
 #print("nodes:", nodes[:]) 
-print("Total Tx:",Tx_total)  
-print("Total Tx try:", Tx_try_sent)  
-throughput = Tx_total/Tx_try_sent 
-print("Throughput:",throughput) 
-print("Throughput (packet_receive/numSlots):", Tx_total/slotframe_size)
-print("Total Rx:",Rx_total)  
+ 
+print("Metrics for TASA") 
 
-print("Tx per node: ",Tx_per_node[:]) 
+print("Tx per node: ",Tx_try_per_node[:]) 
 print("Rx per node: ",Rx_per_node[:])
+Tx_total =  reduce(lambda x, y:x+y, Tx_try_per_node) 
+Rx_total =  reduce(lambda x, y:x+y, Rx_per_node) 
+
+print("Total Tx:",Tx_total)  
+print("Total Rx:",Rx_total)  
+#throughput = Rx_total/slotframe_size 
+#print("Throughput:",throughput)  
+print("PDR(%):",(Rx_total/Tx_total)*100)
+
+print("Metrics for TSCH-LBV:")  
+
+print("Tx per node: ",Tx_try_per_node[:]) 
+print("Rx per node: ",Rx_per_node[:])
+Tx_total =  reduce(lambda x, y:x+y, Tx_try_per_node) 
+Rx_total =  reduce(lambda x, y:x+y, Rx_per_node) 
+
+print("Total Tx:",Tx_total)  
+print("Total Rx:",Rx_total)  
+#throughput = Rx_total/slotframe_size 
+#print("Throughput:",throughput)  
+print("PDR(%):",(Rx_total/Tx_total)*100)
+
