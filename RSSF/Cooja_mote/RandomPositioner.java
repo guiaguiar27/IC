34a42da541b3841f641a0fc952b2cbe75f0648a3
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2021-11-12 09:56:53 -0300

    try the same gen

--- RSSF/Cooja_mote/RandomPositioner.java
+++ RSSF/Cooja_mote/RandomPositioner.java
@@ -17,9 +17,12 @@ public class RandomPositioner extends Positioner {
     private double [] nodesX  = new double[TotalNumNodes]; 
     private double [] nodesY = new double[TotalNumNodes];
     private int levelCheck;   
+    private double level;  
 
     Random rand = new Random(); 
     
+
+
     public RandomPositioner(int totalNumberOfMotes,
 			  double startX, double endX,
 			  double startY, double endY,
@@ -30,28 +33,60 @@ public class RandomPositioner extends Positioner {
     this.endY = endY;
     this.startZ = startZ;
     this.endZ = endZ;  
-    this.TotalNumNodes = totalNumberOfMotes;   
-    int aux_node = totalNumberOfMotes +1; 
-    double level = Math.log(aux_node) / Math.log(2); 
-    System.out.println(level);
-
-
-    //nodes[0][0] = 0.0; 
-    //nodes[1][0] = 0.0;
+    this.TotalNumNodes = totalNumberOfMotes;  
+    this.level = getLevel(totalNumberOfMotes); 
+    Generate_full_binary_tree(this.level, this.startX, this.startY); 
+    for(int i = 0 ; i < TotalNumNodes;i++){ 
+        System.out.print(this.nodesX[i]); 
+        System.out.print(this.nodesY[i]); 
+        System.out.println();
     }
-  public void Generate_full_binary_tree(){  
-        this.node++;  
+    
+
+
+    } 
+    public double getLevel(int nodes){ 
+    int aux_node = nodes +1; 
+    double level = Math.log(aux_node) / Math.log(2); 
+    return level; 
+
+    } 
+
+
+    public void Generate_full_binary_tree(double levels, double nodeX, double nodeY){   
+
+        this.node++;   
+        double lv = levels; 
         double MaxdistanceX = (this.endX - this.startX)/2; 
         double MaxdistanceY = (this.endY - this.startY)/2;    
         double ua,ud,ux,uy;  
 
+        // up child 
         ua = Math.random() * (Math.PI/2);  
         double udX =  Math.random() * MaxdistanceX;   
         double udY =  Math.random() * MaxdistanceX;  
-        ux = this.IposX + udX * Math.cos(ua); 
-        uy = this.IposY + udY * Math.sin(ua);  
-        this.nodesX[node] = ux; 
-        this.nodesY[node] = uy; 
+        ux = nodeX + udX * Math.cos(ua); 
+        uy = nodeY + udY * Math.sin(ua);  
+        this.nodesX[this.node] = ux; 
+        this.nodesY[this.node] = uy;  
+        if (lv != 1){  
+            lv --; 
+            Generate_full_binary_tree(lv, ux,uy);
+        }
+        // down child   
+        this.node ++; 
+        double da,dd,dx,dy; 
+        da = Math.random() * (Math.PI/2) * (-1);  
+        double ddX =  Math.random() * MaxdistanceX;   
+        double ddY =  Math.random() * MaxdistanceX;  
+        dx = nodeX + ddX * Math.cos(ua); 
+        dy = nodeY + ddY * Math.sin(ua);  
+        this.nodesX[this.node] = dx; 
+        this.nodesY[this.node] = dy;  
+        if (lv != 1){  
+            lv --; 
+            Generate_full_binary_tree(lv, dx,dy);
+        }
 
          
   }
