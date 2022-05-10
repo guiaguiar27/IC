9f41213a72407fee29ec038d8b88b179a6da0113
Guilherme Aguiar Silva Milanez <guigasilva27@gmail.com>
2022-05-10 13:05:10 -0300

    just print

--- RSSF/Contiki-NG-Dual-radio/os/net/mac/tsch/tsch-schedule.c
+++ RSSF/Contiki-NG-Dual-radio/os/net/mac/tsch/tsch-schedule.c
@@ -527,7 +527,7 @@ void simple_schedule(){
     if(current_node == 1){  
       linkaddr_t addrRX;   
       int node_origin; 
-     
+      LOG_PRINT("Node 1\n");
       // cria links de recepção para os nós 2 e 3
       node_origin = 2;
       for(int j = 0; j < sizeof(addrRX); j += 2) {
@@ -538,14 +538,14 @@ void simple_schedule(){
                   LINK_OPTION_RX,
                   LINK_TYPE_NORMAL, &addrRX, slot_offset,channel_offset);      
 
-      node_origin = 3;
-      for(int j = 0; j < sizeof(addrRX); j += 2) {
-                addrRX.u8[j + 1] = node_origin & 0xff;
-                addrRX.u8[j + 0] = node_origin >> 8;
-              } 
-      tsch_schedule_add_link(sf_common,
-                  LINK_OPTION_RX,
-                  LINK_TYPE_NORMAL, &addrRX, slot_offset,channel_offset);      
+      // node_origin = 3;
+      // for(int j = 0; j < sizeof(addrRX); j += 2) {
+      //           addrRX.u8[j + 1] = node_origin & 0xff;
+      //           addrRX.u8[j + 0] = node_origin >> 8;
+      //         } 
+      // tsch_schedule_add_link(sf_common,
+      //             LINK_OPTION_RX,
+      //             LINK_TYPE_NORMAL, &addrRX, slot_offset,channel_offset);      
        
 
     } 
@@ -553,7 +553,8 @@ void simple_schedule(){
         int j;
         uint16_t remote_id = 1; 
         linkaddr_t addr;     
-        uint8_t link_options;
+        uint8_t link_options; 
+        LOG_PRINT("Node 2\n");
         for(j = 0; j < sizeof(addr); j += 2) {
           addr.u8[j + 1] = remote_id & 0xff;
           addr.u8[j + 0] = remote_id >> 8;
